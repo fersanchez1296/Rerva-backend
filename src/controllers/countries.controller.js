861063bad33850e15d2460fbef78f01d70e0ba67
fer_sanchez1296 <fernandosanchezplacencia@gmail.com>
2023-10-10 12:32:10 -0600

    controladores y rutas agregads para: 1)getAreas 2)getDisciplinas 3)getCamposDeEstudio 4)getTipoDocumento 5)getEditorial 6)getInstitucion

--- src/controllers/countries.controller.js
+++ src/controllers/countries.controller.js
@@ -42,7 +42,6 @@ export const getCountriesWithCount = async (req, res) => {
   }
 };
 
-
 export const getCountriesData = async (req, res) => {
   const search = req.query.search;
   try {
@@ -95,6 +94,293 @@ export const getCountriesAndDecades = async (req, res) => {
     res.send(error);
   }
 };
+
+export const getCountriesAndAreas = async (req, res) => {
+  try {
+    const allDocuments = await Documents.find().exec();
+
+    // Crear un objeto para realizar el conteo de áreas
+    const areaCounts = {};
+
+    // Iterar sobre los documentos y contar las apariciones de cada área
+    allDocuments.forEach((document) => {
+      const area = document.area; // Reemplaza 'area' con el nombre del campo correcto
+      if (areaCounts[area]) {
+        areaCounts[area] += 1;
+      } else {
+        areaCounts[area] = 1;
+      }
+    });
+
+    // Obtener las áreas distintas
+    const distinctAreas = Object.keys(areaCounts);
+
+    // Crear un array de áreas con sus respectivos conteos
+    const areas = distinctAreas.map((area) => {
+      return { "name_es": area, count: areaCounts[area] };
+    });
+
+    // Ordenar las áreas alfabéticamente si es necesario
+    areas.sort((a, b) => a.name_es.localeCompare(b.name_es));
+
+    const XLabels = areas.map((label) => label.count);
+    const YLabels = areas.map((label) => label["name_es"]);
+
+    // Obtener los países de la publicación como en tu función original
+    const distinctCountries = await Documents.distinct("pais de la publicación").exec();
+    const countries = distinctCountries.map((country) => {
+      return { "name_es": country };
+    });
+
+    // Enviar la respuesta con las áreas y el conteo
+    res.send({ countries, areas, XLabels, YLabels });
+  } catch (error) {
+    res.send(error);
+  }
+};
+
+export const getCountriesAndFieldStudy = async (req, res) => {
+  try {
+    const allDocuments = await Documents.find().exec();
+
+    // Crear un objeto para realizar el conteo de campos
+    const fieldCounts = {};
+
+    // Iterar sobre los documentos y contar las apariciones de cada campo
+    allDocuments.forEach((document) => {
+      const field = document.campo; // Reemplaza 'field' con el nombre del campo correcto
+      if (fieldCounts[field]) {
+        fieldCounts[field] += 1;
+      } else {
+        fieldCounts[field] = 1;
+      }
+    });
+
+    // Obtener los campos distintos
+    const distinctFields = Object.keys(fieldCounts);
+
+    // Crear un array de campos con sus respectivos conteos
+    const fields = distinctFields.map((field) => {
+      return { "name_es": field, count: fieldCounts[field] };
+    });
+
+    // Ordenar los campos alfabéticamente si es necesario
+    fields.sort((a, b) => a.name_es.localeCompare(b.name_es));
+
+    const XLabels = fields.map((label) => label.count);
+    const YLabels = fields.map((label) => label["name_es"]);
+
+    // Obtener los países de la publicación como en tu función original
+    const distinctCountries = await Documents.distinct("pais de la publicación").exec();
+    const countries = distinctCountries.map((country) => {
+      return { "name_es": country };
+    });
+
+    // Enviar la respuesta con los campos y el conteo
+    res.send({ countries, fields, XLabels, YLabels });
+  } catch (error) {
+    res.send(error);
+  }
+};
+
+
+export const getCountriesAndDisciplines = async (req, res) => {
+  try {
+    const allDocuments = await Documents.find().exec();
+
+    // Crear un objeto para realizar el conteo de disciplinas
+    const disciplineCounts = {};
+
+    // Iterar sobre los documentos y contar las apariciones de cada disciplina
+    allDocuments.forEach((document) => {
+      const discipline = document.disciplina; // Reemplaza 'discipline' con el nombre del campo correcto
+      if (disciplineCounts[discipline]) {
+        disciplineCounts[discipline] += 1;
+      } else {
+        disciplineCounts[discipline] = 1;
+      }
+    });
+
+    // Obtener las disciplinas distintas
+    const distinctDisciplines = Object.keys(disciplineCounts);
+
+    // Crear un array de disciplinas con sus respectivos conteos
+    const disciplines = distinctDisciplines.map((discipline) => {
+      return { "name_es": discipline, count: disciplineCounts[discipline] };
+    });
+
+    // Ordenar las disciplinas alfabéticamente si es necesario
+    disciplines.sort((a, b) => a.name_es.localeCompare(b.name_es));
+
+    const XLabels = disciplines.map((label) => label.count);
+    const YLabels = disciplines.map((label) => label["name_es"]);
+
+    // Obtener los países de la publicación como en tu función original
+    const distinctCountries = await Documents.distinct("pais de la publicación").exec();
+    const countries = distinctCountries.map((country) => {
+      return { "name_es": country };
+    });
+
+    // Enviar la respuesta con las disciplinas y el conteo
+    res.send({ countries, disciplines, XLabels, YLabels });
+  } catch (error) {
+    res.send(error);
+  }
+};
+
+
+export const getRegions = async (req,res) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+}
+
+export const getAuthors = async (req,res) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+}
+
+export const getCountriesAndDocumentType = async (req, res) => {
+  try {
+    const allDocuments = await Documents.find().exec();
+
+    // Crear un objeto para realizar el conteo de tipos de documento
+    const documentTypeCounts = {};
+
+    // Iterar sobre los documentos y contar las apariciones de cada tipo de documento
+    allDocuments.forEach((document) => {
+      const documentType = document["tipo de documento"]; // Asegúrate de usar el nombre correcto del campo
+      if (documentTypeCounts[documentType]) {
+        documentTypeCounts[documentType] += 1;
+      } else {
+        documentTypeCounts[documentType] = 1;
+      }
+    });
+
+    // Obtener los tipos de documento distintos
+    const distinctDocumentTypes = Object.keys(documentTypeCounts);
+
+    // Crear un array de tipos de documento con sus respectivos conteos
+    const documentTypes = distinctDocumentTypes.map((documentType) => {
+      return { "name_es": documentType, count: documentTypeCounts[documentType] };
+    });
+
+    // Ordenar los tipos de documento alfabéticamente si es necesario
+    documentTypes.sort((a, b) => a.name_es.localeCompare(b.name_es));
+
+    const XLabels = documentTypes.map((label) => label.count);
+    const YLabels = documentTypes.map((label) => label["name_es"]);
+
+    // Obtener los países de la publicación como en tu función original
+    const distinctCountries = await Documents.distinct("pais de la publicación").exec();
+    const countries = distinctCountries.map((country) => {
+      return { "name_es": country };
+    });
+
+    // Enviar la respuesta con los tipos de documento y el conteo
+    res.send({ countries, documentTypes, XLabels, YLabels });
+  } catch (error) {
+    res.send(error);
+  }
+};
+
+
+export const getCountriesAndEditorial = async (req, res) => {
+  try {
+    const allDocuments = await Documents.find().exec();
+
+    // Crear un objeto para realizar el conteo de libros-editorial
+    const librosEditorialCounts = {};
+
+    // Iterar sobre los documentos y contar las apariciones de cada libro-editorial
+    allDocuments.forEach((document) => {
+      const libroEditorial = document["libros-editorial"]; // Asegúrate de usar el nombre correcto del campo
+      if (librosEditorialCounts[libroEditorial]) {
+        librosEditorialCounts[libroEditorial] += 1;
+      } else {
+        librosEditorialCounts[libroEditorial] = 1;
+      }
+    });
+
+    // Obtener los libros-editorial distintos
+    const distinctLibrosEditorial = Object.keys(librosEditorialCounts);
+
+    // Crear un array de libros-editorial con sus respectivos conteos
+    const librosEditorial = distinctLibrosEditorial.map((libroEditorial) => {
+      return { "name_es": libroEditorial, count: librosEditorialCounts[libroEditorial] };
+    });
+
+    // Ordenar los libros-editorial alfabéticamente si es necesario
+    librosEditorial.sort((a, b) => a.name_es.localeCompare(b.name_es));
+
+    const XLabels = librosEditorial.map((label) => label.count);
+    const YLabels = librosEditorial.map((label) => label["name_es"]);
+
+    // Obtener los países de la publicación como en tu función original
+    const distinctCountries = await Documents.distinct("pais de la publicación").exec();
+    const countries = distinctCountries.map((country) => {
+      return { "name_es": country };
+    });
+
+    // Enviar la respuesta con los libros-editorial y el conteo
+    res.send({ countries, librosEditorial, XLabels, YLabels });
+  } catch (error) {
+    res.send(error);
+  }
+};
+
+// TODO
+// PENDIENTE DE QUE FUNCIONE
+
+export const getCountriesAndInstitution = async (req, res) => {
+  try {
+    const allDocuments = await Documents.find().exec();
+
+    // Crear un objeto para realizar el conteo de tesis-institución
+    const tesisInstitucionCounts = {};
+
+    // Iterar sobre los documentos y contar las apariciones de cada tesis-institución
+    allDocuments.forEach((document) => {
+      const tesisInstitucion = document["tesis-institución"]; // Asegúrate de usar el nombre correcto del campo
+      if (tesisInstitucionCounts[tesisInstitucion]) {
+        tesisInstitucionCounts[tesisInstitucion] += 1;
+      } else {
+        tesisInstitucionCounts[tesisInstitucion] = 1;
+      }
+    });
+
+    // Obtener las tesis-institución distintas
+    const distinctTesisInstitucion = Object.keys(tesisInstitucionCounts);
+
+    // Crear un array de tesis-institución con sus respectivos conteos
+    const tesisInstitucion = distinctTesisInstitucion.map((tesisInstitucion) => {
+      return { "name_es": tesisInstitucion, count: tesisInstitucionCounts[tesisInstitucion] };
+    });
+
+    // Ordenar las tesis-institución alfabéticamente si es necesario
+    tesisInstitucion.sort((a, b) => a.name_es.localeCompare(b.name_es));
+
+    const XLabels = tesisInstitucion.map((label) => label.count);
+    const YLabels = tesisInstitucion.map((label) => label["name_es"]);
+
+    // Obtener los países de la publicación como en tu función original
+    const distinctCountries = await Documents.distinct("pais de la publicación").exec();
+    const countries = distinctCountries.map((country) => {
+      return { "name_es": country };
+    });
+
+    // Enviar la respuesta con las tesis-institución y el conteo
+    res.send({ countries, tesisInstitucion, XLabels, YLabels });
+  } catch (error) {
+    res.send(error);
+  }
+};
+
 
 
 
--- src/routes/countries.routes.js
+++ src/routes/countries.routes.js
@@ -1,5 +1,16 @@
 import { Router } from "express";
-import { getCountries,getCountriesData,getCountriesWithCount,getCountriesAndDecades } from "../controllers/countries.controller.js";
+import {
+  getCountries,
+  getCountriesData,
+  getCountriesWithCount,
+  getCountriesAndDecades,
+  getCountriesAndAreas,
+  getCountriesAndDisciplines,
+  getCountriesAndFieldStudy,
+  getCountriesAndDocumentType,
+  getCountriesAndEditorial,
+  getCountriesAndInstitution
+} from "../controllers/countries.controller.js";
 
 const router = Router();
 
@@ -7,5 +18,11 @@ router.get("/countries", getCountries);
 router.get("/countriesData", getCountriesData);
 router.get("/countriesDataCount", getCountriesWithCount);
 router.get("/countriesAndDecades", getCountriesAndDecades);
+router.get("/countriesAndAreas", getCountriesAndAreas);
+router.get("/countriesAndDisciplines", getCountriesAndDisciplines);
+router.get("/countriesAndFieldStudy", getCountriesAndFieldStudy);
+router.get("/countriesAndDocumentType", getCountriesAndDocumentType);
+router.get("/countriesAndEditorial", getCountriesAndEditorial);
+router.get("/countriesAndInstitution", getCountriesAndInstitution);
 
 export default router;
