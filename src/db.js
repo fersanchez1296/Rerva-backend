9f91d3554f59d723533faa84cc321b8240a87347
root <root@localhost.localdomain>
2024-03-08 13:49:59 -0600

    test bd local

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const URL_CONNNECT = await mongoose.connect("mongodb+srv://rervaCuvalles:RervaCuvalles123@cluster0.0qvo09r.mongodb.net/rerva")
+        const URL_CONNNECT = await mongoose.connect("mongodb://root:root@172.18.0.2:27017/rerva")
         console.log(">>>DB is connected!")
     } catch (error) {
         console.log(error)
