f009a63d69542bfc8fadfd254b4f5191454c0e45
fer_sanchez1296 <fernandosanchezplacencia@gmail.com>
2024-04-25 14:55:38 -0600

    cambio de endpoint a bd para prod

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const URL_CONNNECT = await mongoose.connect("mongodb+srv://rervaCuvalles:RervaCuvalles123@cluster0.0qvo09r.mongodb.net/rerva")
+        const URL_CONNNECT = await mongoose.connect("mongodb://root:root@mongodb:27017/rerva?authSource=admin")
         console.log(">>>DB is connected!")
     } catch (error) {
         console.log(error)
